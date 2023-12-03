2921d8711544a1e956ae11ac25a7d39b19f330b4
JEPTUI <alicejeptui@gmail.com>
2023-12-03 14:40:19 +0300

    MongoDB utils setup

--- utils/db.js
+++ utils/db.js
@@ -6,39 +6,55 @@ class DBClient {
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
 
-    // mongoDB connection URI
+    // MongoDB connection URI
     const uri = `mongodb://${host}:${port}/${database}`;
 
-    // mongoDB client
-    this.clent = new MongoClient(uri, { useUnifiedTopology: true });
-
-    // connect to mongoDB
-    this.connect();
+    this.client = new MongoClient(uri, { useUnifiedTopology: true });
   }
+
   async connect() {
     try {
       await this.client.connect();
+      this.db = this.client.db(this.databaseName);
       console.log('Connected to MongoDB');
-    } catch (err) {
-      console.error('MongoDB connection error:', err);
-      throw err;
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
     }
   }
 
   isAlive() {
-    return this.client.isConnected();
+    try {
+      return this.client.isConnected();
+    } catch (error) {
+      console.error('Error checking MongoDB connection:', error);
+      return false;
+    }
   }
 
   async nbUsers() {
-    const usersCollection = this.client.db().collection('users');
-    return usersCollection.countDocuments();
+    try {
+      const usersCollection = this.db.collection('users');
+      const count = await usersCollection.countDocuments();
+      return count;
+    } catch (error) {
+      console.error('Error counting users:', error);
+      return -1;
+    }
   }
 
   async nbFiles() {
-    const filesCollection = this.client.db().collection('files');
-    return filesCollection.countDocuments();
+    try {
+      const filesCollection = this.db.collection('files');
+      const count = await filesCollection.countDocuments();
+      return count;
+    } catch (error) {
+      console.error('Error counting files:', error);
+      return -1;
+    }
   }
 }
 
 const dbClient = new DBClient();
+dbClient.connect();
+
 export default dbClient;
